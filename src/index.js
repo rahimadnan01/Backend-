be1c12624a7a376e39bfb3530fcfe8e164a2cbb0
Rahim Adnan <hafizrahim3011@gmail.com>
2024-12-30 20:26:28 +0500

    export app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import dotenv from "dotenv";
 import connectDB from "../db/index.js";
+import { app } from "./app.js";
 dotenv.config({ path: `./env` });
 
 connectDB()
   .then(() => {
-    application.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 8000, () => {
       console.log(`server is running on port ${process.env.PORT}`);
     });
   })
