26b6e1b1d51f41de0ad8c822671b4c867f17b126
Rahim Adnan <hafizrahim3011@gmail.com>
2025-01-01 22:21:31 +0500

    adding user router

--- src/app.js
+++ src/app.js
@@ -14,4 +14,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes
+import userRouter from "./routes/user.routes.js";
+
+// router declaration
+app.use("/users", userRouter);
+
 export { app };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import {registerUser} from "../"
 const router = Router();
-
+router.route("/register", registerUser);
 export default router;
